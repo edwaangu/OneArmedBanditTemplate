8b8861d8b18f17987009ae8357ec70b4df585eed
edwaangu <edwaangu171@outlook.com>
2021-05-21 13:08:10 -0400

    Animated it

--- OneArmedBandit/Form1.cs
+++ OneArmedBandit/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace OneArmedBandit
@@ -18,6 +19,9 @@ namespace OneArmedBandit
         // int value for score initialized to 10
 
         int score = 10;
+        int leftReel = 0;
+        int midReel = 0;
+        int rightReel = 0;
 
         public Form1()
         {
@@ -26,56 +30,68 @@ namespace OneArmedBandit
 
         private void spinButton_Click(object sender, EventArgs e) 
         {
-            // get random values for each reel (store each in separate int variable)
-            int leftReel = randGen.Next(1, 4);
-            int midReel = randGen.Next(1, 4);
-            int rightReel = randGen.Next(1, 4);
-
-            // check value of reel 1 with a switch statement to set appropriate image to
-            // BackgroundImage property of pictureBox. Get images from Properties.Resources
-            switch (leftReel)
+            // Go through a for loop of 50
+            for (int i = 0; i < 50; i++)
             {
-                case 1:
-                    pictureBox1.BackgroundImage = Properties.Resources.cherry_100x125;
-                    break;
-                case 2:
-                    pictureBox1.BackgroundImage = Properties.Resources.diamond_100x125;
-                    break;
-                case 3:
-                    pictureBox1.BackgroundImage = Properties.Resources._7_100x125;
-                    break;
-            }
 
-            // check value of reel 2 with a switch statement to set appropriate image to
-            // BackgroundImage property of pictureBox. Get images from Properties.Resources
-            switch (midReel)
-            {
-                case 1:
-                    pictureBox2.BackgroundImage = Properties.Resources.cherry_100x125;
-                    break;
-                case 2:
-                    pictureBox2.BackgroundImage = Properties.Resources.diamond_100x125;
-                    break;
-                case 3:
-                    pictureBox2.BackgroundImage = Properties.Resources._7_100x125;
-                    break;
-            }
+                // get random values for each reel (store each in separate int variable)
+                leftReel = randGen.Next(1, 4);
+                midReel = randGen.Next(1, 4);
+                rightReel = randGen.Next(1, 4);
 
-            // check value of reel 3 with a switch statement to set appropriate image to
-            // BackgroundImage property of pictureBox. Get images from Properties.Resources
-            switch (rightReel)
-            {
-                case 1:
-                    pictureBox3.BackgroundImage = Properties.Resources.cherry_100x125;
-                    break;
-                case 2:
-                    pictureBox3.BackgroundImage = Properties.Resources.diamond_100x125;
-                    break;
-                case 3:
-                    pictureBox3.BackgroundImage = Properties.Resources._7_100x125;
-                    break;
-            }
+                // check value of reel 1 with a switch statement to set appropriate image to
+                // BackgroundImage property of pictureBox. Get images from Properties.Resources
+                switch (leftReel)
+                {
+                    case 1:
+                        pictureBox1.BackgroundImage = Properties.Resources.cherry_100x125;
+                        break;
+                    case 2:
+                        pictureBox1.BackgroundImage = Properties.Resources.diamond_100x125;
+                        break;
+                    case 3:
+                        pictureBox1.BackgroundImage = Properties.Resources._7_100x125;
+                        break;
+                }
 
+                // check value of reel 2 with a switch statement to set appropriate image to
+                // BackgroundImage property of pictureBox. Get images from Properties.Resources
+                switch (midReel)
+                {
+                    case 1:
+                        pictureBox2.BackgroundImage = Properties.Resources.cherry_100x125;
+                        break;
+                    case 2:
+                        pictureBox2.BackgroundImage = Properties.Resources.diamond_100x125;
+                        break;
+                    case 3:
+                        pictureBox2.BackgroundImage = Properties.Resources._7_100x125;
+                        break;
+                }
+
+                // check value of reel 3 with a switch statement to set appropriate image to
+                // BackgroundImage property of pictureBox. Get images from Properties.Resources
+                switch (rightReel)
+                {
+                    case 1:
+                        pictureBox3.BackgroundImage = Properties.Resources.cherry_100x125;
+                        break;
+                    case 2:
+                        pictureBox3.BackgroundImage = Properties.Resources.diamond_100x125;
+                        break;
+                    case 3:
+                        pictureBox3.BackgroundImage = Properties.Resources._7_100x125;
+                        break;
+                }
+
+                pictureBox1.Refresh();
+                pictureBox2.Refresh();
+                pictureBox3.Refresh();
+
+                // Start the pause length at 20, and increase it by 8 each time (to get to 412ms)
+                Thread.Sleep(20 + (i * 8));
+
+            }
 
             /// STOP HERE ----------------------------------------------------------
             /// Test to make sure that your program will display random
